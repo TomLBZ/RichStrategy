3ef9d7654a898ac04a798df7ba913997ee022ffa
TomLBZ <libozhao142857@hotmail.com>
2021-05-09 00:18:25 +0800

    Updated comments

--- RichStrategy/DataAnalyzer.cs
+++ RichStrategy/DataAnalyzer.cs
@@ -19,12 +19,8 @@ namespace RichStrategy
         TF_30S,
         TF_1M,
         TF_5M,
-        TF_10M,
         TF_15M,
-        TF_20M,
-        TF_25M,
         TF_30M,
-        TF_45M,
         TF_1H,
         TF_2H,
         TF_4H,
@@ -55,7 +51,17 @@ namespace RichStrategy
         public DataAnalyzer() { }
 
         public void Analyze() { }
-
+        /// <summary>
+        /// 1、趋势判断从交易时间单位向上一级、两级、三级均相符时（例如，交易时间单位是5分钟，则需要查看15分钟、1小时、8小时），
+        /// 2、且在交易的时间单位上与均线满足一定关系（例如，涨势超过均线，跌时低于均线）时，
+        /// 3、且在交易的时间单位上存在可辨识的方向性的特征（例如旗帜、三角、双底、山形等等）时，
+        /// 4、且在更低一或两级的时间单位（例如交易时间单位时5分钟，低级时间单位是1分钟或30秒）存在局部的入场时机（例如38.2%蜡烛、紧随的蜡烛等等）时，
+        /// 5、根据1中的趋势进行买入/卖出的判断。
+        /// 具体的买卖数额不在这里计算。
+        /// </summary>
+        /// <param name="currentPrice">当前价格</param>
+        /// <param name="currentVolume">当前成交量</param>
+        /// <returns></returns>
         public EvaluationResult EvaluateStep(double currentPrice, double currentVolume) { return EvaluationResult.DISALLOW; }
     }
 }
